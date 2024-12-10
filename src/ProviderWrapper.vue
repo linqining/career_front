56e741e622d4c435d2bf9afd27eb27b5805bf267
linqining <linqining@outlook.com>
2024-12-11 04:18:18 +0800

    suiue provider

---
+++ src/ProviderWrapper.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import App from "@/App.vue";
+import {SuiueProvider} from "suiue";
+</script>
+
+<template>
+  <suiue-provider
+      :config="{
+      requiredFeatures: [
+        'sui:signMessage',
+        'sui:signTransactionBlock',
+        'sui:signAndExecuteTransactionBlock',
+      ],
+      autoConnect: 'enable',
+    }"
+  >
+    <app>
+    </app>
+  </suiue-provider>
+
+</template>
--- src/main.js
+++ src/main.js
@@ -1,18 +1,13 @@
-// import { createApp } from 'vue'
 // import App from './App.vue'
-//
-// createApp(App).mount('#app')
-//
-
 import { createApp } from 'vue';
-import App from './App.vue';
+// import App from './App.vue';
+import ProviderWrapper from "./ProviderWrapper.vue";
 
-// const app = createApp(App);
 // 引入我们导出的 router
 import { setupRouter } from '@/router';
 
 const setupApp = async () => {
-    const app = createApp(App);
+    const app = createApp(ProviderWrapper);
     // 创建路由
     setupRouter(app);
     app.mount('#app');
